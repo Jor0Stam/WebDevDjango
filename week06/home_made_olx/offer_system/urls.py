e533c80a6547e775ca5df141304541a503498533
Jor0Stam <sonik333@mail.bg>
2017-04-11 12:29:52 +0300

    Have to add bootstrap everywhere

--- week06/home_made_olx/offer_system/urls.py
+++ week06/home_made_olx/offer_system/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 
 from .views import OfferListView, CreateOfferView, get_stats_view, OfferUpdate, StatisticsList
 
 
 urlpatterns = [
-    url(r'create-offer/$', CreateOfferView.as_view(), name='create_offer'),
-    url(r'stats/$', StatisticsList.as_view(), name='stats'),
-    url(r'update-offer/(?P<pk>\d+)/$', OfferUpdate.as_view(), name='update_offer'),
+    url(r'create-offer/$',login_required(CreateOfferView.as_view()), name='create_offer'),
+    url(r'stats/$', login_required(StatisticsList.as_view()), name='stats'),
+    url(r'update-offer/(?P<pk>\d+)/$',login_required(OfferUpdate.as_view()), name='update_offer'),
     url(r'^$', OfferListView.as_view(), name='index'),
 ]
